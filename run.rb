2ac86d9310b3b7f32e1bf0238874b4822c6ae43b
Steve <stevendouglaswight@gmail.com>
2013-10-14 23:50:14 -0700

    digging through json response structure

--- run.rb
+++ run.rb
@@ -1,4 +1,6 @@
+require 'rubygems'
 require 'net/http'
+require 'json'
 require './config'
 
 BASE_API = "http://congress.api.sunlightfoundation.com/"
@@ -6,4 +8,23 @@ BASE_API = "http://congress.api.sunlightfoundation.com/"
 url = BASE_API << '/legislators?apikey=' << API_KEY
 uri = URI(url)
 raw = Net::HTTP.get(uri)
-puts raw
+
+parsed = JSON.parse(raw)
+puts "parsed class: #{parsed.class}"
+
+parsed.each do |key,value|
+  puts "key: #{key}"
+end
+
+parsed['results'].each do |key,value|
+  puts "++++++++++++++++++++++++"
+  key.each do |k,v|
+    puts "++++item: #{k}-> #{v}"
+  end  
+  puts "count #{key.length}"
+
+end
+
+puts '----------------------'
+puts "count: #{parsed.length}"
+
